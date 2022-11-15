7d0d996b10385784b30ddd739d647be5ffb0b858
Jaap <info@gekophetweb.nl>
2022-11-15 22:35:18 +0100

    move include to auto prepend file setting

--- public/index.php
+++ public/index.php
@@ -1,9 +1,5 @@
 <?php
 
-    $_GET['setComponents'] = 'develop';
-    require_once("/var/www/vhosts/gekophetweb.nl/components/init.php");
-
-
     use Games\Controller\View;
 
     $view = new View();
